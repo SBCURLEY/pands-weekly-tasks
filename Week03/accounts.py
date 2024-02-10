8be03ff64b5d4247eb2bba28018b13bd42afe724
Sharon Curley <sbrogancurley@gmail.com>
2024-02-10 16:17:26 +0000

    week03

---
+++ Week03/accounts.py
@@ -0,0 +1,13 @@
+# accounts.py
+# This program reads in a 10 character account number and outputs the account number with only the last 4 digits showing
+# Author: Sharon Curley
+
+accountno = input("Please enter a 10 digit account number: ")
+
+# print (accountno[6:])       # prints the last 4 digits as passes over 6
+# print (accountno[:4])     prints the first 4 digits
+
+accountnoX ="XXXXXX"+str(accountno[6:])
+print (accountnoX)
+
+
---
+++ Week03/accounts2.py
@@ -0,0 +1,16 @@
+# accounts2.py
+# This program reads in account numbers of any length and outputs the account number with only the last 4 digits showing
+# Author: Sharon Curley
+
+accountno = input("Please enter an account number: ")
+accountnox = accountno[-4:].rjust(len(accountno),'x')      
+
+# rjust() will right-justify the string within spaces of a given length. 
+# As we do not know the length of the spaces required, we need to count the number using the len function on accountno
+# The program will count the number of digits and replace with x
+# References: A Whrlwind Tour of Python - Formatting strings: Adding and removing space P.71
+# Reference: https://www.w3schools.com/python/ref_string_rjust.asp
+# Reference: A Whrlwind Tour of Python - String Type P.28
+# Reference: https://www.w3schools.com/python/python_strings.asp
+
+print (accountnox)
