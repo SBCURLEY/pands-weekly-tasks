57d670257e16bf24d33e5c258a232cd2d285f49d
Sharon Curley <sbrogancurley@gmail.com>
2024-02-21 07:56:02 +0000

    week02

--- Week02/bank.py
+++ Week02/bank.py
@@ -4,15 +4,13 @@
 
 Amount1 = int (input ("\nEnter Amount 1 (in cent): "))      # Use \n to create a space before the two inout lines.
 Amount2 = int (input ("Enter Amount 2 (in cent): "))        # Line 5 and 6  require the function int before input as the program will see it as a string if not included. 
-answer = float (Amount1 + Amount2)/100
+answer = float (Amount1 + Amount2)/100                      # This is an integer & must be changed to a float by using = float.
 answerFloat="{:.2f}".format(answer)                         # to print 2 decimal places, use str.format() with “{:.2f}” as string and float as a number. 
-                                                            # Reference: https://pythonguides.com/python-print-2-decimal-places/
-
-                                                            # Line 9 is an integer & must be changed to a float by using = float. 
+                                                            # Reference: https://pythonguides.com/python-print-2-decimal-places/ 
                                                             # Reference: w3schools.com - Python Variables - https://www.w3schools.com/python/python_variables.asp
 
 print (f"\nThe sum of Amount 1 and Amount 2 is €{answerFloat}.\n")
 
-                                                            # Line 11 uses an f-string which contains a replacement field {answer} enclosed by curly brackets.  
+                                                            # Line 12 uses an f-string which contains a replacement field {answer} enclosed by curly brackets.  
                                                             # Reference: Real Python - https://realpython.com/python-f-strings/
-                                                            # Line 11 uses \n to create a space before and after the output.
+                                                            # Line 12 uses \n to create a space before and after the output.
